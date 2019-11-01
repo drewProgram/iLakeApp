4e8d59b1fae43cbc8488778aac4be554800d8b6c
Andrew Filgueiras <andrew.filgueiras@outlook.com>
2019-11-01 19:56:33 -0300

    chore: calendar added. Need to make more changes

--- src/pages/Agenda.js
+++ src/pages/Agenda.js
@@ -13,55 +13,94 @@ import {
     Alert
 } from 'react-native';
 
-// meus imports
+// MY IMPORTS
 import MapView from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import CalendarPicker from 'react-native-calendar-picker';
 
 export default function Agenda({ navigation }) {
+    // STATES
     const [loading, setLoading] = useState(true);
     // estado com a localização do usuário
     const [coordinates, setCoordinates] = useState({});
+    const [calendar, setCalendar] = useState({
+        selectedStartDate: null,
+        selectedEndDate: null
+    });
+
+    // VARS 
+    const { selectedStartDate, selectedEndDate } = calendar;
+    const minDate = new Date(); // Today
+    const maxDate = new Date(2017, 6, 3);
+    const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+    const endDate = selectedEndDate ? selectedEndDate.toString() : '';
 
     useEffect(() => {
         // recebe 3 params, sendo o primeiro o que fazer quando tiver sucesso
-        Geolocation.getCurrentPosition(
-            ({ coords }) => {
-                setCoordinates(coords);
-                setLoading(false);
-            }
-        );
-    }, [])
+        Geolocation.getCurrentPosition(({ coords }) => {
+            setCoordinates(coords);
+            setLoading(false);
+        });
+    }, []);
+
+    function onDateChange(date, type) {
+        if (type === 'END_DATE') {
+            setCalendar({
+                selectedEndDate: date,
+            });
+        } else {
+            setCalendar({
+                selectedStartDate: date,
+                selectedEndDate: null,
+            });
+        }
+    }
 
     return (
         <View style={styles.container}>
             <ScrollView>
-            {/* Main page content */}
-            {/* condicional para caso esteja carregando, mostrar o loading, caso contrário, mostrar o mapa */}
-            {/* vamos buscar a localização do usuário usando o hook useEffect */}
-            <View>
-                <Text style={styles.text}>Agenda do evento</Text>
-                <Text style={styles.text}>O que tem por perto?</Text>
-            </View>
-            <View style={{height: 250}}>
-                {loading ? (
-                    <ActivityIndicator size="large" />
-                ) : (
+                {/* Main page content */}
+                {/* condicional para caso esteja carregando, mostrar o loading, caso contrário, mostrar o mapa */}
+                {/* vamos buscar a localização do usuário usando o hook useEffect */}
+                <View>
+                    <Text style={styles.text}>Agenda do evento</Text>
+                    <CalendarPicker
+                        startFromMonday={true}
+                        allowRangeSelection={true}
+                        minDate={minDate}
+                        maxDate={maxDate}
+                        todayBackgroundColor="#f2e6ff"
+                        selectedDayColor="#7300e6"
+                        selectedDayTextColor="#FFFFFF"
+                        onDateChange={onDateChange}
+                    />
 
-                        <MapView
-                            // recebe objeto  como param
-                            // latitudeDelta e longitudeDelta são props para definir o zoom do mapa
-                            initialRegion={{
-                                latitude: coordinates.latitude,
-                                longitude: coordinates.longitude,
-                                latitudeDelta: 0.0068,
-                                longitudeDelta: 0.0068
-                            }}
-                            style={styles.map}
-                        >
-                        </MapView>
+                    <View>
+                        <Text>SELECTED START DATE:{startDate}</Text>
+                        <Text>SELECTED END DATE:{endDate}</Text>
+                    </View>
+                    <Text style={styles.text}>O que tem por perto?</Text>
+                </View>
+                <View style={{ height: 250 }}>
+                    {loading ? (
+                        <ActivityIndicator size="large" />
+                    ) : (
 
-                    )}
-            </View>
+                            <MapView
+                                // recebe objeto  como param
+                                // latitudeDelta e longitudeDelta são props para definir o zoom do mapa
+                                initialRegion={{
+                                    latitude: coordinates.latitude,
+                                    longitude: coordinates.longitude,
+                                    latitudeDelta: 0.0068,
+                                    longitudeDelta: 0.0068
+                                }}
+                                style={styles.map}
+                            >
+                            </MapView>
+
+                        )}
+                </View>
             </ScrollView>
         </View>
     );
